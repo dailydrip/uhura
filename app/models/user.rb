90fb9f629c98832d667ce670f4e2615bfd005e32
Franzé Jr <franzejr@gmail.com>
2019-05-10 19:24:43 -0300

    Fix Bearer token

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,12 @@ class User < ApplicationRecord
   validate :valid_preferences?
 
   def valid_preferences?
-    if self.preferences['email'] && self.preferences['sms']
-      errors.add(:preferences, 'User prefers both Email and SMS, but only one is required.')
-    end
-    if !self.preferences['email'] && !self.preferences['sms']
-      errors.add(:preferences, 'User prefers neither Email and SMS, but one is required.')
-    end
+    # if self.preferences['email'] || self.preferences['sms']
+      # errors.add(:preferences, 'User prefers both Email and SMS, but only one is required.')
+    # end
+    # if !self.preferences['email'] && !self.preferences['sms']
+      # errors.add(:preferences, 'User prefers neither Email and SMS, but one is required.')
+    # end
   end
 
   def opt_in_email
