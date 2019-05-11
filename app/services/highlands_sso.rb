7d1a32d3cd519b446a99c37548b7e8bedffd573a
Franzé Jr <franzejr@gmail.com>
2019-05-11 19:17:30 -0300

    Stub HighlandsSSO preferences method

---
+++ app/services/highlands_sso.rb
@@ -0,0 +1,9 @@
+# This is itended to be the service that
+# communicates with Highlands SSO
+class HighlandsSSO
+  def self.preferred_channel(user)
+    return :email if user.email.include?('smoothterminal')
+
+    :sms
+  end
+end
