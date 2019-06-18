82600496054e3143dcf65b454ab2e67a6035e4fe
Franzé Jr <franzejr@gmail.com>
2019-06-18 09:52:21 -0300

    Delete testlog.rb

--- testlog.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-Dir.chdir '/home/lex/Clients/Concur/Projects/uhura'
-
-printf 'Loading Rails...'
-require './config/environment.rb'
-
-log_puts '1. Seeding Managers (Apps)'
