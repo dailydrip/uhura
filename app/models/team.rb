11f29e6a1438c9bca35cb13a9064f951c1214225
Franzé Jr <franzejr@gmail.com>
2019-06-24 17:30:23 -0300

    Extract error message for team

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class Team < ApplicationRecord
+  def self.error_message_for_team(x_team_id)
+    err_msg = "Team ID (#{x_team_id}) from the X-Team-ID HTTP header NOT found! "
+    err_msg += "Consider adding Team for ID (#{x_team_id}) using the Admin app on the Teams page."
+    err_msg
+  end
 end
