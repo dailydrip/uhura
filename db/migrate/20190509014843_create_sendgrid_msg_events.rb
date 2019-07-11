045e543d7121adc4407fab98a4c2ceace7a125df
l3x <l3x@users.noreply.github.com>
2019-07-11 17:40:45 -0400

    Making ID type consistent (bigint) for sendgrid_msg_event and sendgrid_msg_event tables

--- db/migrate/20190509014843_create_sendgrid_msg_events.rb
+++ db/migrate/20190509014843_create_sendgrid_msg_events.rb
@@ -1,6 +1,6 @@
 class CreateSendgridMsgEvents < ActiveRecord::Migration[6.0]
   def change
-    create_table :sendgrid_msg_events, id: :uuid do |t|
+    create_table :sendgrid_msg_events do |t|
       t.references :sendgrid_msg, null: true, type: :uuid, foreign_key: true
       t.string :status
       t.json :event
